e6d8e9f6f18caf7da67aeb73438018a11797ae5a
Clay Gibson <blackcreekproductions@gmail.com>
2022-07-25 14:02:00 -0400

    basic forms

--- src/components/Layout/ContactSection.vue
+++ src/components/Layout/ContactSection.vue
@@ -4,9 +4,10 @@
 		<form
 			name="contact"
 			method="post"
-			action="submit"
+			action=""
 			data-netlify="true"
 			data-netlify-honeypot="bot-field"
+			netlify
 		>
 			<input
 				type="hidden"
